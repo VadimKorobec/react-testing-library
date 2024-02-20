72c111e0cac03ee9825ef5917b394f528e05e831
Vadim <korobecv2@gmail.com>
2024-02-20 17:49:33 +0200

    add tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const getUser = () => Promise.resolve({ id: 1, name: "Biba" });
+
+const Search = ({ value, onChange, children }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input
+        id="search"
+        type="text"
+        value={value}
+        onChange={onChange}
+        placeholder="search text ..."
+      />
     </div>
   );
-}
+};
+
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [user, setUser] = useState("");
+  console.log(user);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const user = await getUser();
+      setUser(user);
+    };
+    loadUser();
+  }, []);
+
+  const handleChange = ({ target }) => {
+    setSearch(target.value);
+  };
+
+  return (
+    <div>
+      {user && <h2>Logged in as {user.name}</h2>}
+      <img className="image" src="" alt="search image" />
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+      <p>Searhes for {search ? search : "..."}</p>
+    </div>
+  );
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders App component", async () => {
+    render(<App />);
+    await screen.findByText(/Logged in as/i);
+    expect(screen.queryByText(/Searches for React/)).toBeNull();
+    // expect(screen.queryByText(/Searches for React:/i)).toBeNull();
+    // expect(screen.queryByText(/logged in as/i)).toBeNull();
+    // expect( await screen.findByText(/logged in as/i)).toBeInTheDocument();
+    fireEvent.change(screen.getByRole("textbox"), {
+      target: { value: "React" },
+    });
+    expect(screen.getByText(/Searches for React/)).toBeInTheDocument();
+  });
+});
+
+describe("event", () => {
+  it("checkbox click", () => {
+    const handleChange = jest.fn();
+    const { container } = render(
+      <input type="checkbox" onChange={handleChange} />
+    );
+    const checkbox = container.firstChild;
+    expect(checkbox).not.toBeChecked();
+    fireEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+  });
+
+  it("input focus", () => {
+    const { getByTestId } = render(
+      <input type="text" data-testid="simple-input" />
+    );
+    const input = getByTestId("simple-input");
+  });
 });
